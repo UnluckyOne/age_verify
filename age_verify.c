1c39e96af81bd713f51689ff94cbcdbd37780f30
Lxng <54551614+UnluckyOne@users.noreply.github.com>
2022-06-19 15:02:42 +0200

    Add files via upload

---
+++ age_verify.c
@@ -0,0 +1,17 @@
+//
+//  main.c
+//  age_verify
+//
+//  Created by Yanzhe on 2022/6/19.
+//
+
+#include <stdio.h>
+
+int main() {
+    // insert code here...
+    int a;
+    printf("Please input your age.\n");
+    scanf("%d",&a);
+    printf("Thank you, your age is %d. \n",a);
+    return 0;
+}
